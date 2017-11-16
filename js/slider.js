ffa7a229bd20e25cabfece99b07d1b7f456562bd
shenzekun <726664809@qq.com>
2017-11-16 21:34:55 +0800

    彻底完成轮播图无缝链接

--- js/slider.js
+++ js/slider.js
@@ -4,6 +4,7 @@ export class Slider {
         this.slides = options.slides;
         this.interval = options.interval || 3000;
         this.index = 0;
+        this.width = 0;//每张图片的大小的宽度
         this.touchX = 0; //触控开始的手指最初落点
         this.ready_moved = true; //判断每次滑动开始的标记变量
         this.render();
@@ -26,20 +27,37 @@ export class Slider {
         </p>`;
         this.dots = document.querySelector('.ui-slider-dots');
         this.wrap = this.el.firstElementChild;
-        this.wrap.style.width = `${this.slides.length * 100}%`;
+        this.wrap.style.width = `${(this.slides.length + 1) * 100}%`;
         this.wrap.innerHTML = this.slides.map(slide => `<div class="qq-slider-item">
           <a href="${slide.link}">
             <img src="${slide.image}">
           </a>
       </div>`).join('');
+        this.width = this.el.clientWidth
+        let first = this.wrap.firstElementChild;
+        this.wrap.insertAdjacentHTML('beforeend', this.domToString(first)); //无缝链接拷贝第一个到到容器的最后一个
     }
-    
+
+    /**
+     * @description dom 元素转为字符串
+     * @param {any} node 
+     * @returns string
+     * @memberof Slider
+     */
+    domToString (node) {  
+        var tmpNode = document.createElement('div');
+        tmpNode.appendChild(node.cloneNode(true)); 
+        var str = tmpNode.innerHTML;
+        tmpNode = node = null; // 解除引用，以便于垃圾回收  
+        return str  
+    } 
+     
     /**
      * @description 自动播放
      * @memberof Slider
      */
     autoPlay() {
-        this.timer = setInterval(this.next.bind(this), this.interval);
+        this.timer = setInterval(this.next.bind(this), this.interval)
     }
     
     /**
@@ -47,7 +65,7 @@ export class Slider {
      * @memberof Slider
      */
     start() {
-        this.autoPlay();
+        this.autoPlay()
     }
     
     /**
@@ -56,15 +74,34 @@ export class Slider {
      * @memberof Slider
      */
     next() {
-        this.index += 1;
-        if (this.index % this.slides.length === 0) {
-            this.wrap.style.transform = `translate(0)`;
+        if (this.index === this.slides.length) { //5
             this.index = 0;
-            this.setActiveDot();
-            return;
+            this.wrap.style.left = '0';
         }
-        this.wrap.style.transform = `translate(-${this.index * 100 / this.slides.length}%)`;
+        this.index += 1;
+        this.animate(this.wrap,-this.width * this.index)
         this.setActiveDot();
+    }
+
+    /**
+     * @description 动画
+     * @param {any} obj 
+     * @param {any} target 
+     * @memberof Slider
+     */
+    animate(obj, target) {
+        clearInterval(obj.timer);
+        obj.timer = setInterval(function () {
+            var leader = obj.offsetLeft;
+            var step = (target - leader) / 10
+            step = step > 0 ? Math.ceil(step) : Math.floor(step);
+            console.log('step' + step);
+            leader = leader + step;
+            obj.style.left = leader + 'px';
+            if (leader === target) {
+                clearInterval(obj.timer);
+            }
+        }, 30);
     }
 
     /**
@@ -73,32 +110,29 @@ export class Slider {
      * @memberof Slider
      */
     pre() {
-        if (this.index - 1 < 0) {
-            this.index = this.slides.length + this.index - 1;
-        } else {
-            this.index -= 1;
+        if (this.index === 0) {
+            this.index = this.slides.length
+            this.wrap.style.left = `-${this.index * this.width}px`;
         }
-        if (this.index % this.slides.length === 0) {
-            this.wrap.style.transform = `translate(0)`;
-            this.index = 0;
-            this.setActiveDot();
-            return;
-        }
-        this.wrap.style.transform = `translate(-${((this.slides.length + this.index) % this.slides.length) *
-            100 /
-            this.slides.length}%)`;
+        this.index -= 1; 
+        this.animate(this.wrap,-this.index * this.width)
         this.setActiveDot();
     }
+
     /**
      * @description 设置激活的小点
      * @memberof Slider
      */
     setActiveDot() {
         let len = this.dots.children.length;
-        for (let i = 0; i < len; i++) {
+        for (let i = 0; i <= len; i++) {
             let b = document.getElementsByTagName('b')[i];
-            b.classList.remove('active');
+            if (b) b.classList.remove('active');
             if (this.index === i) {
+                if (b) b.classList.add('active');
+            }
+            if (this.index === 5) {
+                b = document.getElementsByTagName('b')[0]
                 b.classList.add('active');
             }
         }
